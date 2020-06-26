4f8ded61469e13467695cc7a5856faa179a97868
EduardoZortea18 <eduardozortea9@gmail.com>
2020-06-26 15:28:02 -0300

    procfile adicionado

---
+++ src/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const server = express();
+
+server.use(express.static("public"));
+
+server.use(express.urlencoded({extended: true}));
+
+const nunjucks = require("nunjucks");
+nunjucks.configure("src/views", {
+    express: server,
+    noCache: true //Not using cache
+});
+
+server.get("/", (req, res) => {
+    return res.render("index.html")
+});
+
+server.get("/euro", (req, res) => {
+    return res.render("euro.html")
+});
+
+server.listen(5000);
